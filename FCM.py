91802814208e46e32b5a239256619b32f2adbec6
Aleix Solanes <al.solanes@gmail.com>
2016-12-28 08:29:22 +0100

    New updated version

--- FCM.py
+++ FCM.py
@@ -1,169 +1,132 @@
-import numpy as np
-from scipy.spatial.distance import cdist
-import numpy as np
+# importing all the required components, you may also use scikit for a direct implementation.
+import copy
+import math
+import random
+import time
+import sys
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import decimal
 
-from sklearn.utils import check_random_state
 
-from . import algorithms
-
-
-def initialize_random(x, k, random_state=None, eps=1e-12):
-    """Selects initial points randomly from the data.
-
-    Parameters
-    ----------
-    x : :class:`np.ndarray`
-        (n_samples, n_features)
-        The original data.
-    k : int
-        The number of points to select.
-    random_state : int or :class:`np.random.RandomState`, optional
-        The generator used for initialization. Using an integer fixes the seed.
-
-    Returns
-    -------
-    Unitialized memberships
-    selection : :class:`np.ndarray`
-        (k, n_features)
-        A length-k subset of the original data.
-
+#used for randomising U
+global MAX
+MAX = 10000.0
+#used for end condition
+global Epsilon
+#Epsilon = 0.00000001
+Epsilon = 0.01
+def print_matrix(list):
     """
-    n_samples = x.shape[0]
-    seeds = check_random_state(random_state).permutation(n_samples)[:k]
-    selection = x[seeds] + eps
-    distances = cdist(x, selection)
-    normalized_distance = distances / np.sum(distances, axis=1)[:, np.newaxis]
-    return 1-normalized_distance, selection
-
-class CMeans:
-    """Base class for C-means algorithms.
-
-    Parameters
-    ----------
-    n_clusters : int, optional
-        The number of clusters to find.
-    n_init : int, optional
-        The number of times to attempt convergence with new initial centroids.
-    max_iter : int, optional
-        The number of cycles of the alternating optimization routine to run for
-        *each* convergence.
-    tol : float, optional
-        The stopping condition. Convergence is considered to have been reached
-        when the objective function changes less than `tol`.
-    verbosity : int, optional
-        The verbosity of the instance. May be 0, 1, or 2.
-
-        .. note:: Very much not yet implemented.
-
-    random_state : :obj:`int` or :obj:`np.random.RandomState`, optional
-        The generator used for initialization. Using an integer fixes the seed.
-    eps : float, optional
-        To avoid numerical errors, zeros are sometimes replaced with a very
-        small number, specified here.
-
-    Attributes
-    ----------
-    metric : :obj:`string` or :obj:`function`
-        The distance metric used. May be any of the strings specified for
-        :obj:`cdist`, or a user-specified function.
-    initialization : function
-        The method used to initialize the cluster centers.
-    centers : :obj:`np.ndarray`
-        (n_clusters, n_features)
-        The derived or supplied cluster centers.
-    memberships : :obj:`np.ndarray`
-        (n_samples, n_clusters)
-        The derived or supplied cluster memberships.
-
+    Prints the matrix in a more reqdable way
     """
+    for i in range(0,len(list)):
+        print list[i]
 
-    metric = 'euclidean'
-    initialization = staticmethod(initialize_random)
+def end_conditon(U,U_old):
+    """
+    This is the end conditions, it happens when the U matrix stops chaning too much with successive iterations.
+    """
+    global Epsilon
+    for i in range(0,len(U)):
+        for j in range(0,len(U[0])):
+            if abs(U[i][j] - U_old[i][j]) > Epsilon :
+                return False
+    return True
 
-    def __init__(self, n_clusters=2, n_init=10, max_iter=300, tol=1e-4,
-                 verbosity=0, random_state=None, eps=1e-18, **kwargs):
-        self.n_clusters = n_clusters
-        self.n_init = n_init
-        self.max_iter = max_iter
-        self.tol = tol
-        self.verbosity = verbosity
-        self.random_state = random_state
-        self.eps = eps
-        self.params = kwargs
-        self.centers = None
-        self.memberships = None
-        if 'metric' in kwargs:
-            self.metric = kwargs['metric']
-        super(Fuzzy, self).__init__(*args, **kwargs)
-        if 'm' in kwargs:
-            self.m = kwargs['m']
-        else:
-            self.m = 2
+def initialise_U(data, cluster_number):
+    """
+    This function would randomis U such that the rows add up to 1. it requires a global MAX.
+    """
+    global MAX
+    U = []
+    for i in range(0,len(data)):
+        current = []
+        rand_sum = 0.0
+        for j in range(0,cluster_number):
+            dummy = random.randint(1,int(MAX))
+            current.append(dummy)
+            rand_sum += dummy
+        for j in range(0,cluster_number):
+            current[j] = current[j] / rand_sum
+        U.append(current)
+    return U
 
-    def distances(self, x):
-        """Calculates the distance between data x and the centers.
-        """
-        return cdist(x, self.centers, metric=self.metric)
+def distance(point, center):
+    """
+    This function calculates the distance between 2 points (taken as a list). We are refering to Eucledian Distance.
+    """
+    if len(point) != len(center):
+        return -1
+    dummy = 0.0
+    for i in range(0,len(point)):
+        dummy += abs(point[i] - center[i]) ** 2
+    return math.sqrt(dummy)
+
+def normalise_U(U):
+    """
+    This de-fuzzifies the U, at the end of the clustering. It would assume that the point is a member of the cluster whoes membership is maximum.
+    """
+    for i in range(0,len(U)):
+        maximum = max(U[i])
+        for j in range(0,len(U[0])):
+            if U[i][j] != maximum:
+                U[i][j] = 0
+            else:
+                U[i][j] = 1
+    return U
 
 
-    def fit(self, x):
-        """Optimizes cluster centers by restarting convergence several times.
-        """
-        objective_best = np.infty
-        memberships_best = None
-        centers_best = None
-        j_list = []
-        for i in range(self.n_init):
-            self.centers = None
-            self.memberships = None
-            self.converge(x)
-            objective = self.objective(x)
-            j_list.append(objective)
-            if objective < objective_best:
-                memberships_best = self.memberships.copy()
-                centers_best = self.centers.copy()
-                objective_best = objective
-        self.memberships = memberships_best
-        self.centers = centers_best
-        return j_list
+def FCM(data, cluster_number, m = 2):
+    """
+    This is the main function, it would calculate the required center, and return the final normalised membership matrix U.
+    It's paramaters are the : cluster number and the fuzzifier "m".
+    """
+    ## initialise the U matrix:
+    U = initialise_U(data, cluster_number)
+    #print_matrix(U)
+    #initilise the loop
+    iteration_num = 0
 
-    def converge(self, x):
-        """Finds cluster centers through an alternating optimization routine.
-        """
-        centers = []
-        j_new = np.infty
-        for i in range(self.max_iter):
-            j_old = j_new
-            self.update(x)
-            centers.append(self.centers)
-            j_new = self.objective(x)
-            if np.abs(j_old - j_new) < self.tol:
-                break
-        return np.array(centers)
+    while (True):
+        #create a copy of it, to check the end conditions
+        U_old = copy.deepcopy(U)
+        # cluster center vector
+        C = []
+        for j in range(0,cluster_number):
+            current_cluster_center = []
+            for i in range(0,len(data[0])): #this is the number of dimensions
+                dummy_sum_num = 0.0
+                dummy_sum_dum = 0.0
+                for k in range(0,len(data)):
+                    print k
+                    dummy_sum_num += (U[k][j] ** m) * data[k][i]
+                    dummy_sum_dum += (U[k][j] ** m)
+                current_cluster_center.append(dummy_sum_num/dummy_sum_dum)
+            C.append(current_cluster_center)
 
-    def update(self, x):
-        """Updates cluster memberships and centers in a single cycle.
-        """
-        self.initialize(x)
-        self.memberships = self.calculate_memberships(x)
-        self.centers = self.calculate_centers(x)
+        #creating a distance vector, useful in calculating the U matrix.
 
-    def initialize(self, x):
-        if self.centers is None and self.memberships is None:
-            self.memberships, self.centers = \
-                self.initialization(x, self.n_clusters, self.random_state)
-        elif self.memberships is None:
-            self.memberships = \
-                self.initialization(x, self.n_clusters, self.random_state)[0]
-        elif self.centers is None:
-            self.centers = \
-                self.initialization(x, self.n_clusters, self.random_state)[1]
+        distance_matrix =[]
+        for i in range(0,len(data)):
+            current = []
+            for j in range(0,cluster_number):
+                current.append(distance(data[i], C[j]))
+            distance_matrix.append(current)
 
-    def fuzzifier(self, memberships):
-        return np.power(memberships, self.m)
+        # update U vector
+        for j in range(0, cluster_number):
+            for i in range(0, len(data)):
+                dummy = 0.0
+                for k in range(0,cluster_number):
+                    dummy += (distance_matrix[i][j]/ distance_matrix[i][k]) ** (2/(m-1))
+                U[i][j] = 1 / dummy
 
-    def objective(self, x):
-        if self.memberships is None or self.centers is None:
-            return np.infty
-        distances = self.distances(x)
-        return np.sum(self.fuzzifier(self.memberships) * distances)
+        if end_conditon(U,U_old):
+            print "finished clustering"
+            break
+        i+=1
+        print 'Iteration:', i
+    U = normalise_U(U)
+    print "normalised U"
+    return U
