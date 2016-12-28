2abfb3a5e141a4ca3213e959430556ae614c4deb
Aleix <al.solanes@gmail.com>
2016-12-28 12:06:24 +0100

    Synthetic data done FCM

--- FCM.py
+++ FCM.py
@@ -1,132 +1,105 @@
-# importing all the required components, you may also use scikit for a direct implementation.
-import copy
-import math
-import random
-import time
-import sys
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import decimal
+import numpy as np
+from sklearn.base import BaseEstimator, ClusterMixin
+from scipy.spatial.distance import cdist
 
+class FuzzyCMeans(BaseEstimator, ClusterMixin):
 
-#used for randomising U
-global MAX
-MAX = 10000.0
-#used for end condition
-global Epsilon
-#Epsilon = 0.00000001
-Epsilon = 0.01
-def print_matrix(list):
-    """
-    Prints the matrix in a more reqdable way
-    """
-    for i in range(0,len(list)):
-        print list[i]
+    def __init__(self,c=2, m=2, max_iterations=1000, error=0.005, init=None, seed=None):
+        self.c= c
+        self.m = m
+        self.max_iterations = 1000
+        self.error = error
+        self.init = init
+        self.seed = seed
 
-def end_conditon(U,U_old):
-    """
-    This is the end conditions, it happens when the U matrix stops chaning too much with successive iterations.
-    """
-    global Epsilon
-    for i in range(0,len(U)):
-        for j in range(0,len(U[0])):
-            if abs(U[i][j] - U_old[i][j]) > Epsilon :
-                return False
-    return True
+    def dist(self,data, centers):
+        return cdist(data, centers).T
 
-def initialise_U(data, cluster_number):
-    """
-    This function would randomis U such that the rows add up to 1. it requires a global MAX.
-    """
-    global MAX
-    U = []
-    for i in range(0,len(data)):
-        current = []
-        rand_sum = 0.0
-        for j in range(0,cluster_number):
-            dummy = random.randint(1,int(MAX))
-            current.append(dummy)
-            rand_sum += dummy
-        for j in range(0,cluster_number):
-            current[j] = current[j] / rand_sum
-        U.append(current)
-    return U
+    def singleStep(self,data, u, c, m):
+        # Normalizing, then eliminating any potential zero values.
+        #u /= np.ones((c, 1)).dot(np.atleast_2d(u_previous.sum(axis=0)))
+        u = np.fmax(u, np.finfo(np.float64).eps)
 
-def distance(point, center):
-    """
-    This function calculates the distance between 2 points (taken as a list). We are refering to Eucledian Distance.
-    """
-    if len(point) != len(center):
-        return -1
-    dummy = 0.0
-    for i in range(0,len(point)):
-        dummy += abs(point[i] - center[i]) ** 2
-    return math.sqrt(dummy)
+        um = u ** m
 
-def normalise_U(U):
-    """
-    This de-fuzzifies the U, at the end of the clustering. It would assume that the point is a member of the cluster whoes membership is maximum.
-    """
-    for i in range(0,len(U)):
-        maximum = max(U[i])
-        for j in range(0,len(U[0])):
-            if U[i][j] != maximum:
-                U[i][j] = 0
-            else:
-                U[i][j] = 1
-    return U
+        data = data.T
 
+        centers = um.dot(data) / (np.ones((data.shape[1], 1)).dot(np.atleast_2d(um.sum(axis=1))).T)
 
-def FCM(data, cluster_number, m = 2):
-    """
-    This is the main function, it would calculate the required center, and return the final normalised membership matrix U.
-    It's paramaters are the : cluster number and the fuzzifier "m".
-    """
-    ## initialise the U matrix:
-    U = initialise_U(data, cluster_number)
-    #print_matrix(U)
-    #initilise the loop
-    iteration_num = 0
+        d = self.dist(data, centers)
+        d = np.fmax(d, np.finfo(np.float64).eps)
 
-    while (True):
-        #create a copy of it, to check the end conditions
-        U_old = copy.deepcopy(U)
-        # cluster center vector
-        C = []
-        for j in range(0,cluster_number):
-            current_cluster_center = []
-            for i in range(0,len(data[0])): #this is the number of dimensions
-                dummy_sum_num = 0.0
-                dummy_sum_dum = 0.0
-                for k in range(0,len(data)):
-                    print k
-                    dummy_sum_num += (U[k][j] ** m) * data[k][i]
-                    dummy_sum_dum += (U[k][j] ** m)
-                current_cluster_center.append(dummy_sum_num/dummy_sum_dum)
-            C.append(current_cluster_center)
+        u = d** (- 2. / (m - 1))
+        u /= np.ones((c, 1)).dot(np.atleast_2d(u.sum(axis=0)))
 
-        #creating a distance vector, useful in calculating the U matrix.
+        return centers, u, d
 
-        distance_matrix =[]
-        for i in range(0,len(data)):
-            current = []
-            for j in range(0,cluster_number):
-                current.append(distance(data[i], C[j]))
-            distance_matrix.append(current)
+    def singleStep_predict(self,data, centers, u_previous, c, m):
 
-        # update U vector
-        for j in range(0, cluster_number):
-            for i in range(0, len(data)):
-                dummy = 0.0
-                for k in range(0,cluster_number):
-                    dummy += (distance_matrix[i][j]/ distance_matrix[i][k]) ** (2/(m-1))
-                U[i][j] = 1 / dummy
+        # Normalizing, then eliminating any potential zero values.
+        #u_previous /= np.ones((c, 1)).dot(np.atleast_2d(u_previous.sum(axis=0)))
 
-        if end_conditon(U,U_old):
-            print "finished clustering"
-            break
-        i+=1
-        print 'Iteration:', i
-    U = normalise_U(U)
-    print "normalised U"
-    return U
+        u_previous = np.fmax(u_previous, np.finfo(np.float64).eps)
+
+        um = u_previous ** m
+        data = data.T
+
+        # For prediction, we do not recalculate cluster centers. The data is
+        # forced to conform to the prior clustering.
+
+        d = self.dist(data, centers)
+        d = np.fmax(d, np.finfo(np.float64).eps)
+
+        u = d ** (- 2. / (m - 1))
+        u /= np.ones((c, 1)).dot(np.atleast_2d(u.sum(axis=0)))
+
+        return u, d
+
+    def fit(self,data):
+        if self.init is None:
+            if self.seed is not None:
+                np.random.seed(self.seed)
+            u0 = np.random.rand(self.c, data.shape[1])
+            u0 /= np.ones((self.c, 1)).dot(np.atleast_2d(u0.sum(axis=0))).astype(np.float64)
+            self.init = u0.copy()
+        u0 = self.init
+        u = np.fmax(u0, np.finfo(np.float64).eps)
+        p= 1
+        while p < self.max_iterations:
+            u2 = u.copy()
+            centers, u, d = self.singleStep(data, u2, self.c, self.m)
+            p += 1
+            if np.linalg.norm(u - u2) < self.error:
+                break
+        # Fuzzy partition Coefficient
+        fpc = np.trace(u.dot(u.T)) / float(u.shape[1])
+
+        return centers, u, u0, d, fpc
+
+    def predict(self, data, trained_centers):
+        c = trained_centers.shape[0]
+
+        # Setup u0
+        if self.init is None:
+            if self.seed is not None:
+                np.random.seed(self.seed)
+            u0 = np.random.rand(c, data.shape[1])
+            u0 /= np.ones((c, 1)).dot(np.atleast_2d(u0.sum(axis=0))).astype(np.float64)
+            self.init = u0.copy()
+        u0 = self.init
+        u = np.fmax(u0, np.finfo(np.float64).eps)
+
+        p = 1
+
+        while p < self.max_iterations:
+            u2 = u.copy()
+            [u, d] = self.singleStep_predict(data, trained_centers, u2, c, self.m)
+            p += 1
+
+            if np.linalg.norm(u - u2) < self.error:
+                break
+
+        # Fuzzy partition Coefficient
+        fpc = np.trace(u.dot(u.T)) / float(u.shape[1])
+
+        return u, u0, d, fpc
